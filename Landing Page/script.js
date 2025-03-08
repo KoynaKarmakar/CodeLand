d6af9ebeb0d9e0498ac90c616cb6cc68a395249f
Koyna Karmakar <166836668+KoynaKarmakar@users.noreply.github.com>
2025-03-08 16:10:56 +0530

    Delete Landing Page directory

--- Landing Page/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("start-btn").addEventListener("click", function() {
-    this.innerText = "Loading... 🚀";
-    setTimeout(() => {
-        window.location.href = "character-selection.html"; // Move to next page
-    }, 2000);
-});
